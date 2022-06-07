dd95df0cf06c1c40cc18329de61e347cb530e341
dandyandy22 <andrasm96@gmail.com>
2022-06-07 11:01:35 -0700

    Apple: Extract string first and middle to methods

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -2,13 +2,21 @@
 # To cure it, apply "extract function".
 
 def story
-  first = "Mr. Dursley was the director of a firm called Grunnings, which made drills."
-  middle = ""
+  first()
+  middle
   5.downto(1) do
     add_new_line(middle)
   end
   conclusion = "And that was it."
   first + middle + conclusion
+end
+
+def first
+  "Mr. Dursley was the director of a firm called Grunnings, which made drills."
+end
+
+def middle
+  ""
 end
 
 def add_new_line(middle)
